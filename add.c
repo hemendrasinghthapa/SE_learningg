c391b3dd4563ca6aeb96eff8c458730a09582b6c
hemendrasinghthapa <shemendra54@gmail.com>
2023-10-19 16:06:41 +0530

    airtmetic program in c

--- add.c
+++ add.c
@@ -2,12 +2,16 @@
 
 void main()
 {
-	int num1;
-	int num2;
-	printf("enter a number\n");
-	scanf("%d",&num1);
-	printf("enter a number\n");
-	scanf("%d",&num2);
-	printf("num1 is  %d num2 is %d",num1,num2);
-	printf("\n number is total %d",num1+num2); 
+ int num1;
+ int num2;
+ printf("enter a number\n");
+ scanf("%d",&num1);
+ printf("enter a number\n");
+ scanf("%d",&num2);
+ printf("\nnum1 is  %d num2 is %d\n",num1,num2);
+ printf("\n add is total %d",num1+num2); 
+ printf("\n multiplication this number %d",num1*num2);
+ printf("\n sub this number %d",num1-num2);
+ printf("\n divison this number %d",num1/num2);
+ printf("\n moduels this number %d",num1%num2);
 }
