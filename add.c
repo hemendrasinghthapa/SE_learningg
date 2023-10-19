7a725b3cc9250d913966874e91f5d0ac0ab46124
hemendrasinghthapa <shemendra54@gmail.com>
2023-10-19 15:47:20 +0530

    airtmetic program in c

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+	int num1;
+	int num2;
+	printf("enter a number\n");
+	scanf("%d",&num1);
+	printf("enter a number\n");
+	scanf("%d",&num2);
+	printf("num1 is  %d num2 is %d",num1,num2);
+	printf("\n number is total %d",num1+num2); 
+}
