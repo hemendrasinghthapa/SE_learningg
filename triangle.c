f300f3e5a6ae60a6c561a04d78e1004893fe102d
hemendrasinghthapa <shemendra54@gmail.com>
2023-10-27 18:10:05 +0530

    task

---
+++ triangle.c
@@ -0,0 +1,70 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	for(int i=0;i<=5;i++){
+//	
+//	for(int j=0;j<=i;j++){
+//		printf("%d",i);
+//	}
+//	printf("\n");
+//	}	
+//}
+
+//			 *
+//			**
+//	       ***
+//		  ****
+//		 *****
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int j;
+//	for(int i=5;i>0;i--){
+//		
+//		for(j=1;j<=i-1;j++){
+//		printf("   ");
+//		}
+//		for(int s=5;s>=j;s--){
+//		printf(" * ");	
+//		
+//		}
+//		printf("\n");
+//	
+//	}	
+//	}	
+	
+						//  Kaju katli  //
+	
+#include<stdio.h>
+int main(){
+	int j;
+	for(int i=10;i>0;i--){
+
+	for(j=1;j<=i-1;j++){
+		printf(" ");
+	}
+	for(int s=10;s>=j;s--){
+		printf(" *");
+} 
+printf("\n");
+}
+
+	for(int i=0;i<9;i++){
+
+	for(j=0;j<=i;j++){
+		printf(" ");
+	}
+	for(int s=9;s>=j;s--){
+		printf(" *");
+} 
+printf("\n");
+	}
+}
+
+
+	
+
